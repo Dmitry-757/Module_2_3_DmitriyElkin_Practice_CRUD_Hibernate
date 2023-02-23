9c73a25c193a300b2aea51b84033cc182a101b7b
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-02-23 23:35:46 +0300

    some variant of getting info from db

--- src/main/java/com/Dmitry_Elkin/PracticeTaskCRUD/repository/Hibernate/HibernateRepository.java
+++ src/main/java/com/Dmitry_Elkin/PracticeTaskCRUD/repository/Hibernate/HibernateRepository.java
@@ -1,8 +1,12 @@
 package com.Dmitry_Elkin.PracticeTaskCRUD.repository.Hibernate;
 
 import com.Dmitry_Elkin.PracticeTaskCRUD.utils.HibernateUtil;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 
 import java.util.List;
 
@@ -48,8 +52,10 @@ public abstract class HibernateRepository<T> {
     public T getById(long id) {
         T item;
         Session session = HibernateUtil.getSession();
-//        session.beginTransaction();
+        session.beginTransaction();
         item = session.find(typeParameterClass, id);
+//        item = session.get(typeParameterClass, id);
+        session.getTransaction().commit();
         session.close();
         return item;
     }
@@ -57,15 +63,19 @@ public abstract class HibernateRepository<T> {
     public List<T> getAll() {
         List<T> itemList;
         Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
         String className = typeParameterClass.getName();
         itemList = session.createQuery("SELECT r FROM "+className+" r", typeParameterClass).getResultList();
 
 //        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
 //        CriteriaQuery<T> criteriaQuery = builder.createQuery(typeParameterClass);
-//        Root<T> root = criteriaQuery.from(typeParameterClass);
+////        Root<T> root = criteriaQuery.from(typeParameterClass);
 //        Query<T> query = session.createQuery(criteriaQuery);
 //        itemList = query.getResultList();
 
+
+        session.getTransaction().commit();
         session.close();
         return itemList;
     }
